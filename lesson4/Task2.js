41c07df9d4f8e2ddff42e035d3f73ebd1460f3ac
tyomka896 <artempronin96@list.ru>
2025-08-04 13:49:28 +0700

    lesson_4_apronin

--- lesson4/Task2.js
+++ lesson4/Task2.js
@@ -1,4 +1,12 @@
-const obj = { prop1: 1, prop2: 2, prop3: true, prop4: 'test', prop5: 10 }
-let resultObj = {}
-...
+const obj = { prop1: 1, prop2: 2, prop3: true, prop4: "test", prop5: 10 };
+let resultObj = {};
+
+for (const [key, value] of Object.entries(obj)) {
+    if (!Number.isInteger(value) || value % 2 != 0) {
+        continue;
+    }
+
+    resultObj[key] = value;
+}
+
 console.log(resultObj); // { prop2: 2, prop5: 10 }
--- lesson4/Task3.js
+++ lesson4/Task3.js
@@ -1,10 +1,25 @@
 const obj = {
-	prop1: null,
-	prop2: {},
-	prop3: 3,
-	prop4: 'str',
-	prop5: 100,
-}
+    prop1: null,
+    prop2: {},
+    prop3: 3,
+    prop4: "str",
+    prop5: 100,
+};
 const result = {};
-...
+
+const incType = (type) => {
+    result[type] ??= 0;
+    result[type]++;
+};
+
+for (const value of Object.values(obj)) {
+    if (value == null || value == undefined) {
+        incType(value);
+
+        continue;
+    }
+
+    incType(typeof value);
+}
+
 console.log(result); // {null: 1, object: 1, string: 1, number: 2}
