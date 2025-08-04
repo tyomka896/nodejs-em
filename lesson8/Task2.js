739858bbf2caf50d0f21732bdb4428ebdd01f064
tyomka896 <artempronin96@list.ru>
2025-08-04 14:26:25 +0700

    lesson_8_apronin

--- lesson8/Task2.js
+++ lesson8/Task2.js
@@ -1,13 +1,17 @@
-const Request1 = () => new Promise((resolve, reject) => {
-	setTimeout(() => {
-		resolve('success');
-	}, 100)
-});
+const Request1 = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("success");
+        }, 100);
+    });
 
-const Request2 = () => new Promise((resolve, reject) => {
-	setTimeout(() => {
-		reject('Ошибка');
-	}, 100)
-});
+const Request2 = () =>
+    new Promise((resolve, reject) => {
+        setTimeout(() => {
+            reject("Ошибка");
+        }, 100);
+    });
 
-...
+await Promise.all([Request1(), Request2()])
+    .then((results) => console.log(`Results on all promises -> ${results}`))
+    .catch((error) => console.log(`Error occurred -> ${error}`));
