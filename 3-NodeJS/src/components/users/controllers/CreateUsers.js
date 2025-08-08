8f8ad1f3dfdad6841081674abeffdd68198b21da
tyomka896 <artempronin96@list.ru>
2025-08-08 15:46:48 +0700

    update and read users controllers

--- 3-NodeJS/src/components/users/controllers/CreateUsers.js
+++ 3-NodeJS/src/components/users/controllers/CreateUsers.js
@@ -1,5 +1,11 @@
 import { CreateUsers } from "../services/CreateUsers.js";
 
+/**
+ * Example:
+ * curl -X POST http://localhost:3000/users \
+ *      -H "Content-Type: application/json" \
+ *      -d '{"name": "Ivan","surname": "Ivanov","password": "securepassword123","email": "ivan.ivanov@example.com"}'
+ */
 export async function CreateUsersController(req, res) {
     const usersData = {
         name: req.body.name,
