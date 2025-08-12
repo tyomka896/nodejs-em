b65ff7a1835bedd488831957b29c5e46a62d2bce
tyomka896 <artempronin96@list.ru>
2025-08-12 16:46:35 +0700

    auth and redis configs

--- 3-NodeJS/src/config.js
+++ 3-NodeJS/src/config.js
@@ -1,5 +1,7 @@
 import Config from "config";
 
+export const AUTH_PASSWORD_KEY = Config.get("AUTH.PASSWORD_KEY");
+
 export const SERVER_PORT = Config.get("SERVER.PORT");
 
 export const DATABASE_HOST = Config.get("DATABASE.HOST");
@@ -11,3 +13,7 @@ export const DATABASE_NAME = Config.get("DATABASE.NAME");
 export const DATABASE_USER = Config.get("DATABASE.USER");
 
 export const DATABASE_PASSWORD = Config.get("DATABASE.PASSWORD");
+
+export const REDIS_ADDRESS = Config.get("REDIS.ADDRESS");
+
+export const REDIS_PORT = Config.get("REDIS.PORT");
