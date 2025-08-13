47c1c1cd170eeb5c1bd81d828c558c6e0a646081
tyomka896 <artempronin96@list.ru>
2025-08-13 23:42:58 +0700

    default config values

--- 3-NodeJS/src/config/database.js
+++ 3-NodeJS/src/config/database.js
@@ -1,11 +1,11 @@
 import Config from "config";
 
-export const DATABASE_HOST = Config.get("DATABASE.HOST");
+export const DATABASE_HOST = Config.get("DATABASE.HOST") || "localhost";
 
-export const DATABASE_PORT = Config.get("DATABASE.PORT");
+export const DATABASE_PORT = Config.get("DATABASE.PORT") || 5432;
 
-export const DATABASE_NAME = Config.get("DATABASE.NAME");
+export const DATABASE_NAME = Config.get("DATABASE.NAME") || "postgres";
 
-export const DATABASE_USER = Config.get("DATABASE.USER");
+export const DATABASE_USER = Config.get("DATABASE.USER") || "postgres";
 
-export const DATABASE_PASSWORD = Config.get("DATABASE.PASSWORD");
+export const DATABASE_PASSWORD = Config.get("DATABASE.PASSWORD") || "postgres";
