78c797f95be2fd8ff94470a878a48db3b3b6c8f0
tyomka896 <artempronin96@list.ru>
2025-08-14 14:05:08 +0700

    check email exists on user creation

--- 3-NodeJS/src/components/users/services/CreateUsers.js
+++ 3-NodeJS/src/components/users/services/CreateUsers.js
@@ -1,8 +1,18 @@
 import sha256 from "#helpers/sha256.js";
 import { connection } from "#libs/database.js";
+import { ValidationError } from "#errors/index.js";
 
 export async function CreateUsersService(usersData) {
     const { name, surname, password, email } = usersData;
+
+    const userExists = await connection.oneOrNone(
+        "SELECT id FROM users WHERE email = $1",
+        [email],
+    );
+
+    if (userExists) {
+        throw new ValidationError("Пользователь с таким email уже существует");
+    }
 
     const hashPassword = sha256(password);
 
